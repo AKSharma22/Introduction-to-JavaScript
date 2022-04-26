6d82ffc9fae76900a062e421b92666a09b3ba5fe
AKSharma22 <99182810+AKSharma22@users.noreply.github.com>
2022-04-26 07:31:55 +0530

    Add files via upload

---
+++ newFile.js
@@ -0,0 +1,26 @@
+// accessing the body element and adding backgroundcolor in it
+document.body.style.backgroundColor = "lightyellow";
+
+// accessing the heading of html and adding text in it
+document.getElementById("head").innerHTML = "JavaScript";
+
+// accessing the heading of html and adding font size
+document.getElementById("head").style.fontSize = "45px";
+
+// accessing the first paragraph and adding text in it
+document.getElementById("p1").innerHTML = "JavaScript is a powerful,flexible, and fast programming language.";
+
+// accessing the second paragraph and adding text in it
+document.getElementById("p2").innerHTML = "Javascript powers the dynamic behavior on website.";
+
+// accessing the third paragraph and adding text in it
+document.getElementById("p3").innerHTML = "Javascript remains at the core of web development.";
+
+// accessing the para 1 and modifing the color of text
+document.getElementById("p1").style.color = "red";
+
+// accessing the para 2 and modifing the color of text
+document.getElementById("p2").style.color = "blue";
+
+// accessing the para 3 and modifing the color of text
+document.getElementById("p3").style.color = "green";
